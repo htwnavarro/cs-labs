2f455448f752e95624cbe354aadab68c3b0b28f7
Heron Thomas Wu-Navarro <herontnavarro@gmail.com>
2024-10-05 20:30:19 -0700

    RM unneeded dir

--- exam_1/src/Jerry.java
+++
@@ -1,6 +0,0 @@
-public class Jerry extends Mary {
-    public void method2() {
-        super.method2();
-        System.out.println("jerry 2");
-    }
-}
--- exam_1/src/Larry.java
+++
@@ -1,6 +0,0 @@
-public class Larry extends Harry {
-    public void method1() {
-        System.out.println("larry 1");
-        super.method1();
-    }
-}
